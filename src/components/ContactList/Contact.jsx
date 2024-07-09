881d03e78b1b9367ad6f0ac6a4a1f397038a1c4a
MichaelTetris <125746441+MichaelTetris@users.noreply.github.com>
2024-07-09 19:39:13 +0300

    add elemet contacts

--- src/components/ContactList/Contact.jsx
+++ src/components/ContactList/Contact.jsx
@@ -1,11 +1,21 @@
 import { FaUser } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
+import css from "./element.module.css";
 
 const Contact = () => {
   return (
-    <div>
-      <FaUser />
-      <FaPhoneAlt />
+    <div className={css.element}>
+      <div>
+        <span className={css.string}>
+          <FaUser />
+          <p>Name</p>
+        </span>
+        <span className={css.string}>
+          <FaPhoneAlt />
+          <p>Phone number</p>
+        </span>
+      </div>
+      <button>Delete</button>
     </div>
   );
 };
